b136724371775573eab9905770eba94d568d7631
Xianyang Qiao <12xq4@queensu.ca>
2016-01-01 21:45:16 -0600

    Try with touch input

---
+++ Assets/MobileInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.tag == "Player")
+                    {
+                        GameObject receiver = hit.transform.gameObject;
+
+                        if (touch.phase == TouchPhase.Began)
+                            receiver.SendMessage("OnTouchDown", SendMessageOptions.DontRequireReceiver);
+                        if (touch.phase == TouchPhase.Ended)
+                            receiver.SendMessage("OnTouchUp", SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+        }
+	}
+}
---
+++ Assets/TouchBehav.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchBehav : MonoBehaviour {
+
+    Animation anim;
+    public AnimationClip idle;
+    public AnimationClip idleSit;
+    public AnimationClip itching;
+    public AnimationClip meow;
+    // Use this for initialization
+    bool interact;
+	void Start () {
+        interact = false;
+        anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!interact)
+            IdleAnim();
+        else
+            InterAnim();
+
+	}
+
+    void OnTouchDown()
+    {
+        interact = true;
+    }
+
+    void OnTouchUp()
+    {
+        interact = false;
+    }
+
+    void IdleAnim()
+    {
+        anim.Play(idle.name);
+        anim.Play(itching.name);
+        anim.Play(idleSit.name);
+    }
+
+    void InterAnim()
+    {
+        anim.Play(meow.name);
+    }
+}
