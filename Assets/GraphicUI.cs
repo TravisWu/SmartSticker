b792f9652d935a6e104b9c6b67cca067379a5d75
12xq4 <12xq4@queensu.ca>
2016-01-13 19:53:02 -0500

    onGUI component

---
+++ Assets/GraphicUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphicUI : MonoBehaviour {
+	public Texture bitMap;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (10, 10, 150, 50), bitMap))
+			Application.OpenURL ("http://lumistories.azurewebsites.net");
+	}
+}
