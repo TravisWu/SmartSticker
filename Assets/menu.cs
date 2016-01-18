6bec11820133992dc17dee586c66eeafb35ccf12
traviswu <travis.wu.bmc@gmail.com>
2016-01-18 09:46:47 -0800

    added UI

---
+++ Assets/menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+		
+		public Texture background;
+		
+		void OnGUI()
+		{
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),background);
+			if (GUI.Button(new Rect(Screen.width / 2-80, Screen.height /2, 150, 25),"Scan")) 
+			{
+				Application.LoadLevel("VR");
+			}
+			
+		}
+	}
