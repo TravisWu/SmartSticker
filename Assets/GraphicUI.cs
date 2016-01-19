4988ca512e48f4cf72ec488c0e9d229e11cce61a
12xq4 <12xq4@queensu.ca>
2016-01-18 19:17:43 -0500

    fixed some UI issues

--- Assets/GraphicUI.cs
+++ Assets/GraphicUI.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class GraphicUI : MonoBehaviour {
 	public Texture bitMap;
+	private float length;
+	private float height;
 
 	// Use this for initialization
 	void Start () {
-	
+		length = 200f;
+		height = 50f;
 	}
 	
 	// Update is called once per frame
@@ -15,7 +18,10 @@ public class GraphicUI : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		if (GUI.Button (new Rect (10, 10, 150, 50), bitMap))
+		if (GUI.Button (new Rect(10,10,50,30),"Back"))
+			Application.LoadLevel ("Main");
+
+		if (GUI.Button (new Rect (Screen.width/2 - length/2, Screen.height - height/2 - 10, length, height), bitMap, GUIStyle.none))
 			Application.OpenURL ("http://lumistories.azurewebsites.net");
 	}
 }
